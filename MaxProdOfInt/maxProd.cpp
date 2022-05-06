7d9b5bb2c14257ab6bb23602366d1e19ad1b54a3
Prashant <iamshaaann@gmail.com>
2022-05-06 22:32:25 +0530

    Array Questions, 1st max product od given array, 2nd max sum subarray

--- MaxProdOfInt/maxProd.cpp
+++ MaxProdOfInt/maxProd.cpp
@@ -1,5 +1,5 @@
 // Given an integer array, find the maximum product of two integers in it.
-// For example, consider array {-10, -3, 5, 6, -2}. The maximum product is the (-10, -3) or (5, 6) pair.
+// For example, consider array {-10, -3, 5, 6, -2}. The maximum product is the (-10, -3) or (5, 6) pair
 
 #include <bits/stdc++.h>
 using namespace std;
